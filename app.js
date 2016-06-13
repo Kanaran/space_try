a2d8a714068be309163727cde51638dc1f526828
Niko Schwarz <schwarz.niko@icloud.com>
2016-06-13 12:36:18 +0200

    nodejs express

--- app.js
+++ app.js
@@ -1,3 +1,16 @@
 /**
  * Created by niko on 09.06.16.
  */
+var express = require('express');
+var app = express();
+var server = require('http').Server(app);
+var io = require('socket.io')(server);
+app.use(express.static('client'));
+server.listen(80);
+
+io.on('connection', function (socket) {
+    socket.emit('news', { hello: 'world' });
+    socket.on('my other event', function (data) {
+        console.log(data);
+    });
+});
